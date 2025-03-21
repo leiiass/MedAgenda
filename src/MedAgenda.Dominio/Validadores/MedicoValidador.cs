64dfad9ae87d212ba9f51d3fc1a11d2c8543b408
Léia Soares <leia.soares@dc.dev.br>
2025-03-21 10:12:40 -0300

    Implementado validações com fluentValidation

---
+++ src/MedAgenda.Dominio/Validadores/MedicoValidador.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using MedAgenda.Dominio.Modelos;
+
+namespace MedAgenda.Dominio.Validadores
+{
+    public class MedicoValidador : AbstractValidator<Medico>
+    {
+        public MedicoValidador()
+        {
+            RuleFor(nome => nome.Nome).NotEmpty().NotNull().WithMessage("Nome do médico é obrigatório.");
+            RuleFor(sobrenome => sobrenome.Sobrenome).NotEmpty().NotNull().WithMessage("O Sobrenome do médico é obrigatório.");
+            RuleFor(numerocrm => numerocrm.NumeroCRM).NotEmpty().NotNull().WithMessage("O número do CRM é obrigatório. ");
+            RuleFor(estadocrm => estadocrm.EstadoCRM).NotEmpty().NotNull().WithMessage("O estado do CRM é obrigatório.");
+            RuleFor(especialidade => especialidade.Especialidade).NotNull().NotEmpty().WithMessage("A especialidade é obrigatória.");
+        }
+    }
+}
---
+++ src/MedAgenda.Dominio/Validadores/PacienteValidador.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using MedAgenda.Dominio.Modelos;
+
+namespace MedAgenda.Dominio.Validadores
+{
+    public class PacienteValidador : AbstractValidator<Paciente>
+    {
+        public PacienteValidador()
+        {
+            RuleFor(nome => nome.Nome).NotEmpty().NotNull().WithMessage("O nome do paciente é obrigatório.");
+            RuleFor(sobrenome => sobrenome.Sobrenome).NotEmpty().NotNull().WithMessage("O sobrenome do paciente é obrigatório.");
+            RuleFor(dataNasc => dataNasc.DataNascimento).NotEmpty().NotNull().WithMessage("A data de nascimento do paciente é obrigatória.");
+            RuleFor(convenio => convenio.Convenio).NotEmpty().NotNull().WithMessage("O convenio do paciente é obrigatório.");
+        }
+    }
+}
